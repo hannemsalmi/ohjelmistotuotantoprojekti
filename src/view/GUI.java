fae98fc9250a707b57fc2c32bccef3d2f94cb867
Katja Peltonen <katja.peltonen3@metropolia.fi>
2023-01-26 17:21:26 +0200

    initial commit

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,6 +1,13 @@
 package view;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class GUI extends Application implements IGUI{
 
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/view/IGUI.java
+++ src/view/IGUI.java
@@ -1,1 +1,5 @@
 package view;
+
+public interface IGUI {
+	
+}
