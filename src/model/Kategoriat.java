bda68ff4798f84d0f0205db085344eb131343cf5
MinaSofi <Minna.Karhu@metropolia.fi>
2023-01-29 13:54:07 +0200

    Päivitetty Kategoriat-luokka toimivampaan kuntoon ja muutettu projekti Maven-projektiksi.

--- src/model/Kategoriat.java
+++ src/model/Kategoriat.java
@@ -1,16 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kategoriat {
 	private List<Kategoria> kategoriat;
+	private Kategoria ruoka;
+	private Kategoria viihde;
+	private Kategoria harrastukset;
+	private Kategoria talouslaskut;
 	
 	public Kategoriat() {
 		kategoriat = new ArrayList<>();
 
 		// Esimerkki aloituskategoriat:
-		kategoriat.add(Ruoka);
-		kategoriat.add(Viihde);
-		kategoriat.add(Harrastukset);
-		kategoriat.add(Talouslaskut);
+		kategoriat.add(ruoka);
+		kategoriat.add(viihde);
+		kategoriat.add(harrastukset);
+		kategoriat.add(talouslaskut);
 	}
 	
 	public void lisääKategoria(Kategoria kategoria) {
