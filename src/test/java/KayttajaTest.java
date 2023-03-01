e7bf1f1ea89be4012403c88228ea8a6f86ee5aab
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-03-01 19:17:07 +0200

    Lisätty JUnit testi käyttäjän lisäämiselle

---
+++ src/test/java/KayttajaTest.java
@@ -0,0 +1,36 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import dataAccessObjects.KayttajaDao;
+import model.Kayttaja;
+
+public class KayttajaTest {
+    private KayttajaDao dao;
+    private Kayttaja kayttaja;
+
+    @Before
+    public void setUp() {
+        dao = new KayttajaDao();
+        kayttaja = new Kayttaja();
+        kayttaja.setNimimerkki("Testi Kayttaja");
+        kayttaja.setMaksimibudjetti(1000.0);
+    }
+
+    @After
+    public void cleanUp() {
+        dao.poistaKayttaja(kayttaja.getKayttajaID());
+    }
+
+    @Test
+    public void testLisaaKayttaja() {
+        dao.lisaaKayttaja(kayttaja);
+        Kayttaja retrievedKayttaja = dao.haeKayttajat(kayttaja.getKayttajaID());
+        assertEquals("Testi Kayttaja", retrievedKayttaja.getNimimerkki());
+        assertEquals(1000.0, retrievedKayttaja.getMaksimibudjetti(), 0.01);
+    }
+}
