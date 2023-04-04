32c405a655434f415e7c5ef17b4d629cd1f93650
Katja Peltonen <katja.peltonen3@metropolia.fi>
2023-04-04 17:21:05 +0300

    Päivitetty Kulut-sisältöikkunaa, nyt toimii muuten paitsi suodatuksessa bugi ja muokkausmahdollisuutta ei vielä

--- src/view/ViewHandler.java
+++ src/view/ViewHandler.java
@@ -1,23 +1,39 @@
 package view;
 
 import java.io.IOException;
+import java.util.List;
 
+import controller.IKontrolleri;
+import controller.Kontrolleri;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import kayttajanHallinta.KayttajanHallinta;
+import model.Kulu;
 
 //Täältä löytyvät kaikki metodit, joita kutsutaan eri controllereista
-public class ViewHandler {
+public class ViewHandler implements IGUI{
+	private IKontrolleri kontrolleri;
 	private Stage stage;
 	private ViewController aktiivinen;
 	private BorderPane root;
 	private AnchorPane sisalto;
 	
+	private KayttajanHallinta kayttajanhallinta = KayttajanHallinta.getInstance();
+
 	public ViewHandler(Stage stage) {
 		this.stage = stage;
+		kontrolleri = new Kontrolleri(this);
 	}
 	
 	public void start() {
@@ -43,6 +59,7 @@ public class ViewHandler {
 		}
 		
 		naytaEtusivu();
+		onkoKayttajaa();
 	}
 	
 	public void naytaEtusivu() {
@@ -82,5 +99,58 @@ public class ViewHandler {
 		}
 	}
 	
+	public void onkoKayttajaa() {
+		if (kontrolleri.getKayttaja(1) == null) {
+			StackPane root = new StackPane();
+            Scene scene = new Scene(root, 400, 400);
+            Stage kayttajaStage = new Stage();
+
+            root.setStyle("-fx-background-color: #DAE3E5;");
+
+            Label label = new Label("Luo uusi käyttäjätunnus:");
+            TextField textField = new TextField();
+            Label label2 = new Label("Aseta kuukausittainen budjettisi:");
+            TextField textField2 = new TextField();
+            Button button = new Button("Luo käyttäjä");
+            button.setStyle("-fx-background-color: #507DBC; -fx-text-fill: white; -fx-font-weight: bold;");
+            label.setStyle("-fx-font-family: Arial;");
+            textField.setStyle("-fx-font-family: Arial;");
+            label2.setStyle("-fx-font-family: Arial;");
+            textField2.setStyle("-fx-font-family: Arial;");
+
+            VBox vbox = new VBox();
+            vbox.setSpacing(10);
+            vbox.setPadding(new Insets(10));
+            vbox.getChildren().addAll(label, textField, label2, textField2, button);
+            root.getChildren().add(vbox);
+            
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    String username = textField.getText();
+                    double budjetti = Double.parseDouble(textField2.getText());
+                    if (!username.isEmpty()) {
+                        kontrolleri.lisaaKayttaja(username, budjetti);
+                        kontrolleri.lisaaKategoria("Yleinen", username);
+                        kayttajanhallinta.kirjoitaKayttajaID(1);
+                        kayttajanhallinta.setKirjautunutKayttaja(kontrolleri.getKayttaja(kayttajanhallinta.lueKayttajaID()));
+                        kayttajaStage.close();
+                    }
+                }
+            });
+            kayttajaStage.setScene(scene);
+            kayttajaStage.setTitle("Luo käyttäjä");
+    	    kayttajaStage.show();
+		}
+		
+		kayttajanhallinta.setKirjautunutKayttaja(kontrolleri.getKayttaja(kayttajanhallinta.lueKayttajaID()));
+	}
 	
+	public KayttajanHallinta getKayttajanhallinta() {
+		return kayttajanhallinta;
+	}
+
+	public IKontrolleri getKontrolleri() {
+		return kontrolleri;
+	}
 }
