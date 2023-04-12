89e016841e6ddc0cb15ca0bf822ce848b00cf9eb
Katja Peltonen <katja.peltonen3@metropolia.fi>
2023-04-12 11:18:50 +0300

    Lisätty suodatusominaisuus

--- src/view/DiagrammiController.java
+++ src/view/DiagrammiController.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,13 +53,18 @@ public class DiagrammiController implements ViewController {
 	private ViewHandler vh;
 	private KayttajanHallinta kayttajanhallinta;
 	private List<Kulu> kaikkiKulut;
+	private List <Kulu> suodatetut;
+	
 	
 	@Override
 	public void init(ViewHandler viewHandler) {
 		vh = viewHandler;
 		kayttajanhallinta = vh.getKayttajanhallinta();
 		kaikkiKulut = vh.getKontrolleri().getKulut(kayttajanhallinta.lueKayttajaID());
+		suodatetut = kaikkiKulut;
+		initSuodatus();
 		naytaKuluDiagrammi();
+		laskeKulutusYhteensa();
 	}
 	
 	public void naytaKuluDiagrammi() {
@@ -66,7 +73,7 @@ public class DiagrammiController implements ViewController {
 	    ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
 
 	    Map<Kategoria, Double> kuluSums = new HashMap<>();
-	    for (Kulu kulu : kaikkiKulut) {
+	    for (Kulu kulu : suodatetut) {
 	        Kategoria kategoria = kulu.getKategoria();
 	        double sum = kuluSums.getOrDefault(kategoria, 0.0);
 	        kuluSums.put(kategoria, sum + kulu.getSumma());
@@ -84,5 +91,59 @@ public class DiagrammiController implements ViewController {
 	    pieChart.setLabelLineLength(50);
 	    pieChart.setLabelsVisible(true);
 	    pieChart.setLegendVisible(false);
+	}
+	
+	public void suodataAika() {
+	    int valittuKuukausi = kuukausiBox.getSelectionModel().getSelectedIndex();
+	    int valittuVuosiIndeksi = vuosiBox.getSelectionModel().getSelectedIndex();
+	    int valittuVuosi = LocalDate.now().getYear() - valittuVuosiIndeksi + 1;
+	    
+	    List<Kulu> suodatetutKulut = new ArrayList<>();
+	    
+	    if (valittuKuukausi == 0 && valittuVuosiIndeksi == 0){
+	        suodatetutKulut.addAll(kaikkiKulut);
+	      }
+	    else {
+	    	  for (Kulu kulu : kaikkiKulut) {
+	    		  if ((kulu.getPaivamaara().getMonthValue() == valittuKuukausi || valittuKuukausi == 0 )&& (kulu.getPaivamaara().getYear() == valittuVuosi || valittuVuosiIndeksi == 0) ) {
+	    			  suodatetutKulut.add(kulu);
+		        }
+		    }
+	    }
+		suodatetut = suodatetutKulut;
+		naytaKuluDiagrammi();
+		laskeKulutusYhteensa();
+	}
+	
+	public void laskeKulutusYhteensa() {
+		double kokonaiskulutus = 0;
+		
+		for (Kulu kulu : suodatetut) {
+			kokonaiskulutus += kulu.getSumma();
+		}
+		
+		kulutus.setText("Kulutus yhteensä:\n" + String.format("%.2f",kokonaiskulutus ) + " €");
+	}
+	
+	public void initSuodatus() {
+		kuukausiBox.getItems().add("Kaikki");
+		kuukausiBox.getItems().add("Tammi");
+		kuukausiBox.getItems().add("Helmi");
+		kuukausiBox.getItems().add("Maalis");
+		kuukausiBox.getItems().add("Huhti");
+		kuukausiBox.getItems().add("Touko");
+		kuukausiBox.getItems().add("Kesä");
+		kuukausiBox.getItems().add("Heinä");
+		kuukausiBox.getItems().add("Elo");
+		kuukausiBox.getItems().add("Syys");
+		kuukausiBox.getItems().add("Loka");
+		kuukausiBox.getItems().add("Marras");
+		kuukausiBox.getItems().add("Joulu");
+		kuukausiBox.getSelectionModel().select("Kaikki");
+		vuosiBox.getItems().add("Kaikki");
+		for (int i = LocalDate.now().getYear(); i >= LocalDate.now().getYear() - 5; i--) {
+			vuosiBox.getItems().add(Integer.toString(i));
+	    }
+		vuosiBox.getSelectionModel().select("Kaikki");
 	}
 }
