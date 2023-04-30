63289c55a0f9ad4c3e3bc77876ab7834e07be867
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-04-30 18:57:58 +0300

    Muokattu @author tageja

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /**
  * A main class used for launching the original graphical user interface.
  * The updated version of the interface can be launched with MainView class.
- * @authors hannemsalmi, willeKoodaus, Katanpe, MinaSofi
+ * @author hannemsalmi, willeKoodaus, Katanpe, MinaSofi
  */
 public class Main {
 	public static void main(String[] args) {
